2f59f649604ef463a39fd291e4ec08fafa1bba3b
mcarlomagno <marcoscarlomagno1@gmail.com>
2021-02-11 20:47:13 -0300

    :sparkles: feat: geolocation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  googleMapsApiKey: 'AIzaSyB6r7SNWLG3NVqU6DDCOlKAoqAlbkG597Q'
 };
 
 /*
